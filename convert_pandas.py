243fdd6344211c89ad4c2b0eae2879be46679db9
ver8ung <45457029+ver8ung@users.noreply.github.com>
2023-02-15 18:11:35 +0100

    Update convert_pandas.py

--- convert_pandas.py
+++ convert_pandas.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-dict1 = {"A701" : "Agilent_Primer_Pair_1_I7",
-"A702" : "Agilent_Primer_Pair_2_I7",
-"A703" : "Agilent_Primer_Pair_3_I7",
-"A704" : "Agilent_Primer_Pair_4_I7",
-"A705" : "Agilent_Primer_Pair_5_I7",
-"A706" : "Agilent_Primer_Pair_6_I7",
-"A707" : "Agilent_Primer_Pair_7_I7",
-"A708" : "Agilent_Primer_Pair_8_I7",
-"A709" : "Agilent_Primer_Pair_9_I7"
+dict1 = {"a" : {"A701" : {"Agilent_Primer_Pair_1_I7"}},
+        {"a" : {"A702" : {"Agilent_Primer_Pair_2_I7"}},
+        {"a" : {"A703" : "Agilent_Primer_Pair_3_I7"}},
+        {"a" : {"A704" : "Agilent_Primer_Pair_4_I7"}},
+        {"a" : {"A705" : "Agilent_Primer_Pair_5_I7"}},
+        {"a" : {"A706" : "Agilent_Primer_Pair_6_I7"}},
+        {"a" : {"A707" : "Agilent_Primer_Pair_7_I7"}},
+        {"a" : {"A708" : "Agilent_Primer_Pair_8_I7"}},
+        {"a" : {"A709" : "Agilent_Primer_Pair_9_I7"}}               
 }
 
 input = pd.read_excel("test_sample_sheet.xlsx")
